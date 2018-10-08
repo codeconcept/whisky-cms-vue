3fad583ea4d3bf2f9015e94a753a4288c66950dd
codeconcept <contact@codeconcept.fr>
2018-10-08 14:57:03 +0200

    adding vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+import '@babel/polyfill';
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+import './plugins/vuetify';
 import App from './App.vue';
 
+
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
 
 new Vue({
   render: h => h(App),
