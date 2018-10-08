87a5d8f6293fca78971e343f18f54132b1012706
codeconcept <contact@codeconcept.fr>
2018-10-08 15:27:42 +0200

    vuetify card

--- src/App.vue
+++ src/App.vue
@@ -31,9 +31,49 @@
     <v-content>
       <v-container fluid fill-height>
         <v-layout>
-          <cc-main-header></cc-main-header>
           <v-flex text-xs-center>
-            
+          <cc-main-header></cc-main-header>
+          <!-- card begin -->
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-card>
+                <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                  height="200px"
+                >
+                </v-img>
+
+                <v-card-title primary-title>
+                  <div>
+                    <div class="headline">Etre programmeur ça pique parfois</div>
+                    <span class="grey--text">ce cactus est pas là pour rien</span>
+                  </div>
+                </v-card-title>
+
+                <v-card-actions>
+                  <v-btn flat>Partager</v-btn>
+                  <v-btn flat color="purple">Explorer</v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn icon @click="show = !show">
+                    <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                  </v-btn>
+                </v-card-actions>
+
+                <v-slide-y-transition>
+                  <v-card-text v-show="show">
+                    I'm a thing. But, like most politicians, he promised more than he could deliver.
+                    You won't have time for sleeping, soldier,
+                    not with all the bed making you'll be doing.
+                    Then we'll go with that data file!
+                    Hey, you add a one and two zeros to that or we walk!
+                    You're going to do his laundry? I've got to find a way to escape.
+                  </v-card-text>
+                </v-slide-y-transition>
+              </v-card>
+            </v-flex>
+            </v-layout>
+          <!-- card end -->
+
           </v-flex>
         </v-layout>
       </v-container>
@@ -54,6 +94,7 @@ export default {
   },
   data: () => ({
     drawer: null,
+    show: false,
   }),
   props: {
     source: String,
