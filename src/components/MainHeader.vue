f71c2e12523379fb88ce9550856260ad95b079c8
codeconcept <contact@codeconcept.fr>
2018-10-08 14:36:08 +0200

    first commit

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Ma vie de programmeur</h2>
+    <h3>avec des morceaux de code dedans</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
