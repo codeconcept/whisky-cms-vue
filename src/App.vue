15006c136e6671ba1551706aa53b3d7074ffa687
codeconcept <contact@codeconcept.fr>
2018-10-08 15:16:04 +0200

    adding responsive vuetify theme

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,47 @@
 <template>
-  <div id="app">
-    <cc-main-header></cc-main-header>
-  </div>
+  <v-app id="app">
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Accueil</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>MVDP</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout>
+          <cc-main-header></cc-main-header>
+          <v-flex text-xs-center>
+            
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2018</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -12,11 +52,16 @@ export default {
   components: {
     ccMainHeader: MainHeader,
   },
+  data: () => ({
+    drawer: null,
+  }),
+  props: {
+    source: String,
+  },
 };
 </script>
 
 <style>
 #app {
-  margin-top: 60px;
 }
 </style>
