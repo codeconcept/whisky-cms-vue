eb5372487fa337fd90da52da318334069738374a
codeconcept <contact@codeconcept.fr>
2018-10-14 12:10:31 +0200

    retrieving blog posts from API

--- src/App.vue
+++ src/App.vue
@@ -50,6 +50,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import MainHeader from './components/MainHeader.vue';
 import BlogCard from './components/BlogCard.vue';
 
@@ -62,31 +63,16 @@ export default {
   data: () => ({
     drawer: null,
     show: false,
-    posts: [
-      {
-        id: 1,
-        title: 'Etre programmeur ça pique parfois',
-        subTitle: 'ce cactus est pas là pour rien',
-        image: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg',
-        content:
-          "I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier. not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.",
-      },
-      {
-        id: 2,
-        title: 'Un nouveau framework JS',
-        subTitle: 'encore !?',
-        image: 'https://fakeimg.pl/300/?text=frameworks',
-        content: 'Ca en fait 1 par semaine. On va pas tenir le rythme',
-      },
-      {
-        id: 3,
-        title: 'Vive le vanilla JavaScript',
-        subTitle: 'encore !?',
-        image: 'https://fakeimg.pl/300/?text=JS',
-        content: 'Là au moins, on est sûr que ça va durer',
-      },
-    ],
+    posts: [],
   }),
+  created() {
+    axios
+      .get('http://localhost:8081/api/v1/posts')
+      .then((result) => {
+        this.posts = result.data;
+      })
+      .catch(err => console.error(err));
+  },
   methods: {
     toDo() {
       console.log('todo');
